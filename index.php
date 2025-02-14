c0e001b83c750bae8e57d5a68d8c2d27b23a918e
Regular001(1053) <kabriacid01@gmail.com>
2025-02-14 02:51:28 +0100

    created the reusable variables, stylesheet pages and imported them successfully

--- index.php
+++ index.php
@@ -3,18 +3,25 @@ session_start();
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dataspeed - Home</title>
-    <link rel="stylesheet" href="public/assets/css/bootstrap.min.css">
+    <link rel="shortcut icon" href="public/logo.svg" type="image/x-icon">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap-grid.min.css">
     <link rel="stylesheet" href="public/assets/css/style.css">
 </head>
 
 <body>
     <?php require __DIR__ . "/public/partials/navbar.php"; ?>
-    <main>
-
+    <main class="container">
+        <section class="mt-5"></section>
+        <section class="jumbotoron">
+            <h1 id="main-header">The cheapest and most <br>affordable <span class="primary">airtime</span> data <br>plug.</h1>
+            <p id="header-para">Convert your airtime to cash, make & receive payments,<br> instant airtime and data
+                delivery, make bill payments <br> with DataSpeed. The all-in-one payments app.</p>
+        </section>
     </main>
 </body>
 
--- public/partials/header.php
+++ public/partials/header.php
@@ -1,21 +1,12 @@
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
-        <link rel="stylesheet" href="style.css">
-    </head>
-<header>
-    <div class="logo">DataSpeed</div>
-    <nav>
-        <a href="#">Home</a>
-        <a href="#">Products</a>
-        <a href="#">Pricing</a>
-    </nav>
-    <div class="auth-buttons">
-        <a href="login.php" class="login-btn">Login</a>
-        <a href="register.php" class="signup-btn">Create an account</a>
-    </div>
-</header>
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DataSpeed</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap-grid.min.css">
+    <link rel="stylesheet" href="style.css">
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+
+</head>
