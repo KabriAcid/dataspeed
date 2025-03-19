bce1af9f7f7f6752c0f79a185fdbcbef35ef21ab
Regular001(1053) <kabriacid01@gmail.com>
2025-03-19 06:41:10 +0100

    commit

--- public/partials/header.php
+++ public/partials/header.php
@@ -10,7 +10,6 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap-utilities.min.css">
     <!-- Add font awesome icons to buttons (note that the fa-spin class rotates the icon) -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="../../assets/css/dashboard.css">
     <link rel="stylesheet" href="../../assets/css/style.css">
 
     <style>
