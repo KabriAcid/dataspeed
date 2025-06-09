d8022ad21323d028145774480d141b16ffd4e51f
Abdullahi Kabri <kabriacid01@gmail.com>
2025-06-09 22:30:20 +0100

    feat: update pinpad modal structure and styling for improved user experience

--- public/partials/pinpad_modal.php
+++ public/partials/pinpad_modal.php
@@ -38,16 +38,18 @@
         <button type="button" class="key-button" data-value="9">9</button>
       </div>
       <div class="keypad-row">
-        <button type="button" class="key-spacer" disabled></button>
+        <button type="button" class="key-button invisible"></button>
         <button type="button" class="key-button" data-value="0">0</button>
-        <button type="button" id="backspace" class="key-backspace" aria-label="Backspace">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-7 h-7"><path fill-rule="evenodd" d="M2.515 10.674a1.875 1.875 0 000 2.652L8.89 19.7c.352.351.829.549 1.326.549H19.5a3 3 0 003-3V6.75a3 3 0 00-3-3h-9.284c-.497 0-.974.198-1.326.55l-6.375 6.374zM12.53 9.22a.75.75 0 10-1.06 1.06L13.19 12l-1.72 1.72a.75.75 0 101.06 1.06l1.72-1.72 1.72 1.72a.75.75 0 101.06-1.06L15.31 12l1.72-1.72a.75.75 0 10-1.06-1.06l-1.72 1.72-1.72-1.72z" clip-rule="evenodd"></path></svg>
+        <button type="button" id="backspace" class="key-button key-backspace" aria-label="Backspace">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#1A202C" style="width: 1.1em; height: 1.1em;">
+            <path fill-rule="evenodd" d="M2.515 10.674a1.875 1.875 0 000 2.652L8.89 19.7c.352.351.829.549 1.326.549H19.5a3 3 0 003-3V6.75a3 3 0 00-3-3h-9.284c-.497 0-.974.198-1.326.55l-6.375 6.374zM12.53 9.22a.75.75 0 10-1.06 1.06L13.19 12l-1.72 1.72a.75.75 0 101.06 1.06l1.72-1.72 1.72 1.72a.75.75 0 101.06-1.06L15.31 12l1.72-1.72a.75.75 0 10-1.06-1.06l-1.72 1.72-1.72-1.72z" clip-rule="evenodd"></path>
+          </svg>
         </button>
       </div>
     </div>
     <div class="pin-action-buttons d-flex justify-content-between mt-3">
-      <button type="button" id="pin-logout-btn">Logout</button>
-      <button type="button" id="pin-forgot-btn">Forgot PIN</button>
+      <button type="button" id="pin-exit-btn"><i class="fa fa-home"></i> Exit</button>
+      <button type="button" id="pin-forgot-btn"><i class="fa fa-question-mark"></i> Forgot PIN</button>
     </div>
   </div>
 </div>
