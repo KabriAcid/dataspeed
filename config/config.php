4ba58e21c660753a03fb2c5a6d20023f7f325363
KabriAcid <kabriacid01@gmail.com>
2025-07-30 17:08:47 +0100

    Enable error display for development: set display_errors to 1 and log_errors to 0

--- config/config.php
+++ config/config.php
@@ -2,8 +2,8 @@
 date_default_timezone_set('Africa/Lagos');
 
 // Error Reporting log
-ini_set('display_errors', 0);
-ini_set('log_errors', 1);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 define('ACCOUNT_STATUS_ACTIVE', 101);
